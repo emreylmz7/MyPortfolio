c915f55cecbe834abc99dc203234e002c60249a6
emreylmz7 <yilmazvemre@gmail.com>
2025-01-17 23:37:46 +0300

    spinner added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from 'react';
 import {
   Main,
   Timeline,
@@ -10,35 +10,52 @@ import {
 } from "./components";
 import FadeIn from './components/FadeIn';
 import './index.scss';
-
+import Spinner from './components/Spinner';
 
 function App() {
     const [mode, setMode] = useState<string>('dark');
+    const [isLoading, setIsLoading] = useState(true); // Yükleme durumu
 
     const handleModeChange = () => {
-        if (mode === 'dark') {
-            setMode('light');
-        } else {
-            setMode('dark');
-        }
-    }
+        setMode(mode === 'dark' ? 'light' : 'dark');
+    };
 
     useEffect(() => {
-        window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
-      }, []);
+        const images = Array.from(document.images); // Sayfadaki tüm img etiketlerini alın
+        const imagePromises = images.map((img) => 
+            new Promise((resolve) => {
+                if (img.complete) {
+                    resolve(true);
+                } else {
+                    img.onload = () => resolve(true);
+                    img.onerror = () => resolve(true);
+                }
+            })
+        );
+
+        Promise.all(imagePromises).then(() => {
+            setIsLoading(false); // Tüm görseller yüklendi
+        });
+
+        window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+    }, []);
 
     return (
-    <div className={`main-container ${mode === 'dark' ? 'dark-mode' : 'light-mode'}`}>
-        <Navigation parentToChild={{mode}} modeChange={handleModeChange}/>
-        <FadeIn transitionDuration={700}>
-            <Main/>
-            <Expertise/>
-            <Timeline/>
-            <Project/>
-            <Contact mode={mode} />
-        </FadeIn>
-        <Footer />
-    </div>
+        isLoading ? (
+            <Spinner />
+        ) : (
+            <div className={`main-container ${mode === 'dark' ? 'dark-mode' : 'light-mode'}`}>
+                <Navigation parentToChild={{ mode }} modeChange={handleModeChange} />
+                <FadeIn transitionDuration={700}>
+                    <Main />
+                    <Expertise />
+                    <Timeline />
+                    <Project />
+                    <Contact mode={mode} />
+                </FadeIn>
+                <Footer />
+            </div>
+        )
     );
 }
 
---
+++ src/components/Spinner.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/styles/Spinner.scss';
+
+const Spinner = () => {
+  return (
+    <div className="spinner">
+      <div></div>
+    </div>
+  );
+};
+
+export default Spinner;
